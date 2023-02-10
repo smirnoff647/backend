c1460659248c03dd20674ae4031fc8c006a8937c
smir noff <smirnoff@dev.looneyloops.de>
2023-02-10 19:47:48 +0100

    uibuild

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.use(express.static('build'))
+
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let notes = [  
     {
