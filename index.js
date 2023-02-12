47cfe69facaea81bda5fe29642df4275d2d856dc
smir noff <smirnoff@dev.looneyloops.de>
2023-02-12 17:26:53 +0100

    fixed prodction build of frontend

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!!!</h>')
-// })
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!!!</h>')
+})
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
