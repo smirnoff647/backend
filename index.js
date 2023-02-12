d2d5ce365115f8a59a820a8261a6d1d27851b7da
smir noff <smirnoff@dev.looneyloops.de>
2023-02-12 14:57:13 +0100

    added MongoDB

--- index.js
+++ index.js
@@ -1,84 +1,86 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
+const Note = require('./models/note')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
-
-let notes = [  
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    { 
-        id: 2, 
-        content: "Browser can execute only JavaScript", 
-        important: false 
-    }, 
-    {
-        id: 3, 
-        content: "GET and POST are the most important methods of HTTP protocol", 
-        important: true 
-    }
-]
+app.use(express.json())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!!!</h>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note =  notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+   Note.findById(request.params.id)
+   .then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+   })
+   .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save()
+        .then(savedNote => response.json(savedNote))
+        .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important} = request.body
+
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        { content, important }, 
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
